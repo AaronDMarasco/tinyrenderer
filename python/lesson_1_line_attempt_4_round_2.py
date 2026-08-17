00320e9683c541c547e1ee0070f646739ad711f6
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-16 21:08:05 -0400

    Round 2

---
+++ python/lesson_1_line_attempt_4_round_2.py
@@ -0,0 +1,76 @@
+from __future__ import annotations
+
+import sys
+from typing import Final
+
+import numpy as np
+
+from lib.tgaimage import TGAColor, TGAColor_t, TGAImage
+
+white: Final = TGAColor(255, 255, 255, 255).resize(bpp=3)  # Attention: BGRA order
+green: Final = TGAColor(0, 255, 0, 255).resize(bpp=3)
+red: Final = TGAColor(0, 0, 255, 255).resize(bpp=3)
+blue: Final = TGAColor(255, 128, 64, 255).resize(bpp=3)
+yellow: Final = TGAColor(0, 200, 255, 255).resize(bpp=3)
+
+
+def line(ax: int, ay: int, bx: int, by: int, framebuffer: TGAImage, color: TGAColor_t) -> None:
+    steep: Final = abs(ax - bx) < abs(ay - by)
+    if steep:
+        ax, ay = ay, ax
+        bx, by = by, bx
+    if ax > bx:  # make it left-to-right
+        ax, bx = bx, ax
+        ay, by = by, ay
+    y: float = ay
+    if bx == ax:  # BUG: C++ allowed the div by zero here?
+        bx += 1
+    y_inc: Final = (by - ay) / (bx - ax)
+    for x in range(ax, min(bx + 1, framebuffer.width)):
+        y += y_inc
+        fixed_y = min(round(y), framebuffer.height - 1)
+        if fixed_y > 63:
+            raise ValueError(fixed_y)
+        if steep:  # if transposed, de-transpose
+            framebuffer.set(fixed_y, x, color)
+        else:
+            framebuffer.set(x, fixed_y, color)
+
+
+def main() -> int:
+    width: Final = 64
+    height: Final = 64
+
+    framebuffer = TGAImage(width, height, TGAImage.Format.RGB)
+
+    _CPP_SOURCE = """
+    std::srand(std::time({}));
+    for (int i=0; i<(1<<24); i++) {
+        int ax = rand()%width, ay = rand()%height;
+        int bx = rand()%width, by = rand()%height;
+        line(ax, ay, bx, by, framebuffer, { rand()%255, rand()%255, rand()%255, rand()%255 });
+    }
+    """
+
+    rng = np.random.default_rng()  # seed=42)
+    for _ in range(1 << 18):  # 4.0s (LOL - C++ and Python optimize differently!)
+        ax = int(rng.integers(width))
+        ay = int(rng.integers(width))
+        bx = int(rng.integers(width))
+        by = int(rng.integers(width))
+        line(
+            ax,
+            ay,
+            bx,
+            by,
+            framebuffer,
+            TGAColor(int(rng.integers(255)), int(rng.integers(255)), int(rng.integers(255))),
+        )
+
+    framebuffer.write_tga_file("framebuffer.tga")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
