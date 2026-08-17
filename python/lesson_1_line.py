5a9e8a14a65b4dd40f52621c69bc6f146d07b443
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-16 20:11:25 -0400

    Lesson 1: Lines

---
+++ python/lesson_1_line.py
@@ -0,0 +1,96 @@
+from __future__ import annotations
+
+import sys
+from typing import Final
+
+import numpy as np
+
+from lib.tgaimage import TGAColor, TGAColor_t, TGAImage
+
+_CPP_SOURCE = """
+#include <cmath>
+#include "tgaimage.h"
+
+constexpr TGAColor white   = {255, 255, 255, 255}; // attention, BGRA order
+constexpr TGAColor green   = {  0, 255,   0, 255};
+constexpr TGAColor red     = {  0,   0, 255, 255};
+constexpr TGAColor blue    = {255, 128,  64, 255};
+constexpr TGAColor yellow  = {  0, 200, 255, 255};
+
+void line(int ax, int ay, int bx, int by, TGAImage &framebuffer, TGAColor color) {
+    for (float t=0.; t<1.; t+=.02) {
+        int x = std::round( ax + (bx-ax)*t );
+        int y = std::round( ay + (by-ay)*t );
+        framebuffer.set(x, y, color);
+    }
+}
+
+int main(int argc, char** argv) {
+    constexpr int width  = 64;
+    constexpr int height = 64;
+    TGAImage framebuffer(width, height, TGAImage::RGB);
+
+    int ax =  7, ay =  3;
+    int bx = 12, by = 37;
+    int cx = 62, cy = 53;
+
+    line(ax, ay, bx, by, framebuffer, blue);
+    line(cx, cy, bx, by, framebuffer, green);
+    line(cx, cy, ax, ay, framebuffer, yellow);
+    line(ax, ay, cx, cy, framebuffer, red);
+
+    framebuffer.set(ax, ay, white);
+    framebuffer.set(bx, by, white);
+    framebuffer.set(cx, cy, white);
+
+    framebuffer.write_tga_file("framebuffer.tga");
+    return 0;
+}
+"""
+
+white: Final = TGAColor(255, 255, 255, 255).resize(bpp=3)  # Attention: BGRA order
+green: Final = TGAColor(0, 255, 0, 255).resize(bpp=3)
+red: Final = TGAColor(0, 0, 255, 255).resize(bpp=3)
+blue: Final = TGAColor(255, 128, 64, 255).resize(bpp=3)
+yellow: Final = TGAColor(0, 200, 255, 255).resize(bpp=3)
+
+
+def line(ax: int, ay: int, bx: int, by: int, framebuffer: TGAImage, color: TGAColor_t) -> None:
+    # for t in np.arange(stop=1.0, step=0.02):  # Should this be linspace?
+    for t in np.linspace(start=0.0, stop=1.0, num=50, endpoint=False):
+        x = round(ax + (bx - ax) * t)
+        y = round(ay + (by - ay) * t)
+        framebuffer.set(x, y, color)
+
+
+def main() -> int:
+    width: Final = 64
+    height: Final = 64
+
+    framebuffer = TGAImage(width, height, TGAImage.Format.RGB)
+
+    ax = 7
+    ay = 3
+    bx = 12
+    by = 37
+    cx = 62
+    cy = 53
+
+    line(ax, ay, bx, by, framebuffer, blue)
+    line(cx, cy, bx, by, framebuffer, green)
+    line(cx, cy, ax, ay, framebuffer, yellow)
+    line(ax, ay, cx, cy, framebuffer, red)
+
+    # A difference in the python vs C++ version is that python only stores valid data,
+    # so the BPP of the pixel must match the image. If not, you will get a warning, e.g.:
+    framebuffer.set(ax, ay, white)  # "Pixel write at (7, 3) changed bpp"
+    framebuffer.set(bx, by, white.resize(bpp=3))
+    framebuffer.set(cx, cy, white.resize(bpp=3))
+
+    framebuffer.write_tga_file("framebuffer.tga")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
