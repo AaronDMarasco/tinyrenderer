23ae2c527f6908a2d4216a0221ccee911ae814b1
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-16 23:29:43 -0400

    objreader: don't scale

--- python/lib/objreader.py
+++ python/lib/objreader.py
@@ -73,8 +73,7 @@ def read_obj_file(infile: str | Path) -> OBJ_Data:
     with open(infile, encoding="utf-8") as ifile:
         for line_no, line in enumerate(ifile, 1):
             if m := VERTEX_RE.match(line):
-                # They go from -1 to 1 so normalize to 0..1
-                res.add_vertex(OBJ_Vertex((float(m["x"]) + 1) / 2, (float(m["y"]) + 1) / 2, (float(m["z"]) + 1) / 2))
+                res.add_vertex(OBJ_Vertex(float(m["x"]), float(m["y"]), float(m["z"])))
             elif m := FACE_RE.match(line):
                 f0 = OBJ_Face_entry(vertex=int(m["vertex_0"]), texture=int(m["texture_0"]), normal=int(m["normal_0"]))
                 f1 = OBJ_Face_entry(vertex=int(m["vertex_1"]), texture=int(m["texture_1"]), normal=int(m["normal_1"]))
