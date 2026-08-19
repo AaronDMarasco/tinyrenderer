68859710cc52bcbe1870fb6d8acdab50cc377fa1
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-19 17:04:41 -0400

    Start homework (lesson 3)

---
+++ python/lesson_3_homework.py
@@ -0,0 +1,28 @@
+from __future__ import annotations
+
+import sys
+from typing import Final
+
+from lib.render import triangle_barycentric_lesson_3
+from lib.tgaimage import TGAColor, TGAImage
+
+white: Final = TGAColor(255, 255, 255, 255).resize(bpp=3)  # Attention: BGRA order
+green: Final = TGAColor(0, 255, 0, 255).resize(bpp=3)
+red: Final = TGAColor(0, 0, 255, 255).resize(bpp=3)
+blue: Final = TGAColor(255, 128, 64, 255).resize(bpp=3)
+yellow: Final = TGAColor(0, 200, 255, 255).resize(bpp=3)
+
+
+def main() -> int:
+    width: Final = 64
+    height: Final = 64
+
+    framebuffer = TGAImage(width, height, TGAImage.Format.GRAYSCALE)
+    triangle_barycentric_lesson_3((17, 4, 13), (55, 39, 128), (23, 59, 255), framebuffer)
+    framebuffer.write_tga_file("framebuffer.tga")
+
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(main())
