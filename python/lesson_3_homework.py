fcbe4e4c09516c0f58d18f5faa9acb53959fd202
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-19 17:14:58 -0400

    Homework was easier than expected.

--- python/lesson_3_homework.py
+++ python/lesson_3_homework.py
@@ -3,7 +3,7 @@ from __future__ import annotations
 import sys
 from typing import Final
 
-from lib.render import triangle_barycentric_lesson_3
+from lib.render import triangle_barycentric_lesson_3_homework
 from lib.tgaimage import TGAColor, TGAImage
 
 white: Final = TGAColor(255, 255, 255, 255).resize(bpp=3)  # Attention: BGRA order
@@ -17,8 +17,8 @@ def main() -> int:
     width: Final = 64
     height: Final = 64
 
-    framebuffer = TGAImage(width, height, TGAImage.Format.GRAYSCALE)
-    triangle_barycentric_lesson_3((17, 4, 13), (55, 39, 128), (23, 59, 255), framebuffer)
+    framebuffer = TGAImage(width, height, TGAImage.Format.RGB)
+    triangle_barycentric_lesson_3_homework((17, 4, 13), (55, 39, 128), (23, 59, 255), framebuffer)
     framebuffer.write_tga_file("framebuffer.tga")
 
     return 0
