cf8c32adc5549b1844ab6d70fee119cb1b156c10
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-21 22:09:06 -0400

    vec2 and vec4 types as well as all testing harnesses for them

--- python/lib/tests/test_trtypes.py
+++ python/lib/tests/test_trtypes.py
@@ -1,73 +1,114 @@
 #!/bin/env python
 from __future__ import annotations
 
-import os
-import sys
-from collections.abc import Callable, Generator
-from pathlib import Path
-from typing import Final, NamedTuple, Self
+from typing import Self, TypeAlias
 
 import numpy as np
-
-# import numpy.typing as npt
 import pytest
-from hypothesis import assume, given
+from hypothesis import given
 from hypothesis import strategies as st
 
-from ..trtypes import vec3
+from ..trtypes import _VectorBase, vec2, vec3, vec4
+
+reasonable_integers = st.integers(min_value=-(2**31), max_value=2**31 - 1)
+
+VecParam: TypeAlias = tuple[int, type[_VectorBase]]
 
 
-class TestVec3:
-    def test_to_from_numpy(self: Self) -> None:
-        uut = vec3(1, 2, 3)
-        from icecream import ic
+@pytest.mark.parametrize("vec_param", [(2, vec2), (3, vec3), (4, vec4)], ids=["vec2", "vec3", "vec4"])
+class TestVector:
+    @given(vec_in=st.lists(reasonable_integers, min_size=4, max_size=4))
+    def test_to_from_numpy(self: Self, vec_in: list[int], vec_param: VecParam) -> None:
+        width, class_ = vec_param
+        vec_in = vec_in[:width]
 
-        ic(uut.np)
+        uut = class_(*vec_in)
         uut2 = uut.from_np(uut.np)
         assert isinstance(uut.np, np.ndarray)
-        assert uut.np.shape == (3,)
+        assert uut.np.shape == (width,)
         assert uut.np.dtype == float
         assert uut == uut
         assert isinstance(uut2.x, float)  # Not an np type
         assert isinstance(uut2.y, float)
-        assert isinstance(uut2.z, float)
-        assert uut2.y == 2
+        if width >= 3:
+            assert isinstance(uut2, (vec3, vec4))
+            assert isinstance(uut2.z, float)
+        if width >= 4:
+            assert isinstance(uut2, vec4)
+            assert isinstance(uut2.a, float)
 
-    def test_from_numpy_bad_type(self: Self) -> None:
-        with pytest.raises(AssertionError):
-            vec3.from_np([1, 2, 3])  # type: ignore[arg-type]
+        assert uut2.y == vec_in[1]
 
-    def test_from_numpy_wrong_size(self: Self) -> None:
-        with pytest.raises(AssertionError):
-            vec3.from_np(np.array([]))
-        with pytest.raises(AssertionError):
-            vec3.from_np(np.array([[1, 2, 3], [4, 5, 6]], dtype=float))
+    def test_from_numpy_bad_type(self: Self, vec_param: VecParam) -> None:
+        width, class_ = vec_param
 
-    def test_from_numpy_wrong_type(self: Self) -> None:
         with pytest.raises(AssertionError):
-            vec3.from_np(np.array([1, 2, 3], dtype=int))
+            class_.from_np([0] * width)  # type: ignore[arg-type]
 
-    @given(vec_in=st.lists(st.floats(allow_nan=False, allow_infinity=False), min_size=6, max_size=6))
-    def test_add(self: Self, vec_in: list[float]) -> None:
-        uut1 = vec3(*vec_in[0:3])
-        uut2 = vec3(*vec_in[3:6])
+    @given(vec_in=st.lists(reasonable_integers, min_size=4, max_size=4))
+    def test_from_numpy_wrong_size(self: Self, vec_in: list[int], vec_param: VecParam) -> None:
+        width, class_ = vec_param
+        vec_in = vec_in[:width]
+
+        with pytest.raises(AssertionError):
+            class_.from_np(np.array([]))
+        with pytest.raises(AssertionError):
+            class_.from_np(np.array([vec_in, vec_in], dtype=float))
+
+    @given(vec_in=st.lists(reasonable_integers, min_size=4, max_size=4))
+    def test_from_numpy_wrong_type(self: Self, vec_in: list[int], vec_param: VecParam) -> None:
+        width, class_ = vec_param
+
+        with pytest.raises(AssertionError):
+            class_.from_np(np.array(vec_in[:width], dtype=int))
+
+    @given(vec_in=st.lists(st.floats(allow_nan=False, allow_infinity=False), min_size=8, max_size=8))
+    def test_add(self: Self, vec_in: list[float], vec_param: VecParam) -> None:
+        width, class_ = vec_param
+        # vec_in = vec_in[: (width * 2)]
+
+        uut1 = class_(*vec_in[0:width])
+        uut2 = class_(*vec_in[width : 2 * width])
         res = uut1 + uut2
-        assert res.x == pytest.approx(vec_in[0] + vec_in[3])
-        assert res.y == pytest.approx(vec_in[1] + vec_in[4])
-        assert res.z == pytest.approx(vec_in[2] + vec_in[5])
+        assert res.x == pytest.approx(vec_in[0] + vec_in[width])
+        assert res.y == pytest.approx(vec_in[1] + vec_in[width + 1])
+        if width >= 3:
+            assert isinstance(res, (vec3, vec4))
+            assert res.z == pytest.approx(vec_in[2] + vec_in[width + 2])
+        if width >= 4:
+            assert isinstance(res, vec4)
+            assert res.a == pytest.approx(vec_in[3] + vec_in[width + 3])
 
-    @given(vec_in=st.lists(st.floats(allow_nan=False, allow_infinity=False), min_size=6, max_size=6))
-    def test_sub(self: Self, vec_in: list[float]) -> None:
-        uut1 = vec3(*vec_in[0:3])
-        uut2 = vec3(*vec_in[3:6])
+    @given(vec_in=st.lists(st.floats(allow_nan=False, allow_infinity=False), min_size=8, max_size=8))
+    def test_sub(self: Self, vec_in: list[float], vec_param: VecParam) -> None:
+        width, class_ = vec_param
+
+        uut1 = class_(*vec_in[0:width])
+        uut2 = class_(*vec_in[width : 2 * width])
         res = uut1 - uut2
-        assert res.x == pytest.approx(vec_in[0] - vec_in[3])
-        assert res.y == pytest.approx(vec_in[1] - vec_in[4])
-        assert res.z == pytest.approx(vec_in[2] - vec_in[5])
+        assert res.x == pytest.approx(vec_in[0] - vec_in[width])
+        assert res.y == pytest.approx(vec_in[1] - vec_in[width + 1])
+        if width >= 3:
+            assert isinstance(res, (vec3, vec4))
+            assert res.z == pytest.approx(vec_in[2] - vec_in[width + 2])
+        if width >= 4:
+            assert isinstance(res, vec4)
+            assert res.a == pytest.approx(vec_in[3] - vec_in[width + 3])
 
-    @given(vec_in=st.lists(st.floats(allow_nan=False, allow_infinity=False, width=32), min_size=6, max_size=6))
-    def test_dot_product(self: Self, vec_in: list[float]) -> None:
-        uut1 = vec3(*vec_in[0:3])
-        uut2 = vec3(*vec_in[3:6])
+    @given(vec_in=st.lists(st.floats(allow_nan=False, allow_infinity=False, width=32), min_size=8, max_size=8))
+    def test_dot_product(self: Self, vec_in: list[float], vec_param: VecParam) -> None:
+        width, class_ = vec_param
+
+        uut1 = class_(*vec_in[0:width])
+        uut2 = class_(*vec_in[width : 2 * width])
         res = uut1 * uut2
-        assert res == pytest.approx(uut1.x * uut2.x + uut1.y * uut2.y + uut1.z * uut2.z)
+        expected = uut1.x * uut2.x + uut1.y * uut2.y
+        if width >= 3:
+            assert isinstance(uut1, (vec3, vec4))
+            assert isinstance(uut2, (vec3, vec4))
+            expected += uut1.z * uut2.z
+        if width >= 4:
+            assert isinstance(uut1, vec4)
+            assert isinstance(uut2, vec4)
+            expected += uut1.a * uut2.a
+        assert res == pytest.approx(expected)
--- python/lib/trtypes.py
+++ python/lib/trtypes.py
@@ -2,14 +2,19 @@ from __future__ import annotations
 
 from abc import ABC, abstractmethod
 from dataclasses import dataclass
-from typing import Self
+from typing import Self  # , TypeVar
 
 import numpy  # Import as np conflicts with property named np
 import numpy.typing as npt
 
+# VB = TypeVar("VB", bound="_VectorBase")
+
 
 @dataclass(frozen=True, slots=True)
 class _VectorBase(ABC):
+    x: float
+    y: float
+
     @property
     @abstractmethod
     def array(self: Self) -> list[float]:
@@ -20,35 +25,94 @@ class _VectorBase(ABC):
         return numpy.array(self.array, dtype=float)
 
     @staticmethod
-    def from_np(array: npt.NDArray[numpy.float64]) -> vec3:
-        assert isinstance(array, numpy.ndarray)
-        assert array.shape == (3,)
-        assert array.dtype == float
-        return vec3(x=float(array[0]), y=float(array[1]), z=float(array[2]))
+    @abstractmethod
+    def from_np(array: npt.NDArray[numpy.float64]) -> _VectorBase: ...
 
-    def __mul__(self: Self, other: vec3) -> float:
+    @abstractmethod
+    def __add__(self: Self, other: _VectorBase) -> _VectorBase: ...
+
+    @abstractmethod
+    def __sub__(self: Self, other: _VectorBase) -> _VectorBase: ...
+
+    def __mul__(self: Self, other: _VectorBase) -> float:
         """Dot product"""
-        if not isinstance(other, vec3):
+        if not isinstance(other, _VectorBase):
             return NotImplemented
         return numpy.dot(self.array, other.array)
 
 
 @dataclass(frozen=True, slots=True)
+class vec2(_VectorBase):
+    @property
+    def array(self: Self) -> list[float]:
+        return [self.x, self.y]
+
+    @staticmethod
+    def from_np(array: npt.NDArray[numpy.float64]) -> vec2:
+        assert isinstance(array, numpy.ndarray)
+        assert array.shape == (2,)
+        assert array.dtype == float
+        return vec2(x=float(array[0]), y=float(array[1]))
+
+    def __add__(self: Self, other: _VectorBase) -> vec2:
+        if not isinstance(other, vec2):
+            return NotImplemented
+        return vec2(self.x + other.x, self.y + other.y)
+
+    def __sub__(self: Self, other: _VectorBase) -> vec2:
+        if not isinstance(other, vec2):
+            return NotImplemented
+        return vec2(self.x - other.x, self.y - other.y)
+
+
+@dataclass(frozen=True, slots=True)
 class vec3(_VectorBase):
-    x: float
-    y: float
     z: float
 
     @property
     def array(self: Self) -> list[float]:
         return [self.x, self.y, self.z]
 
-    def __add__(self: Self, other: vec3) -> vec3:
+    @staticmethod
+    def from_np(array: npt.NDArray[numpy.float64]) -> vec3:
+        assert isinstance(array, numpy.ndarray)
+        assert array.shape == (3,)
+        assert array.dtype == float
+        return vec3(x=float(array[0]), y=float(array[1]), z=float(array[2]))
+
+    def __add__(self: Self, other: _VectorBase) -> vec3:
         if not isinstance(other, vec3):
             return NotImplemented
         return vec3(self.x + other.x, self.y + other.y, self.z + other.z)
 
-    def __sub__(self: Self, other: vec3) -> vec3:
+    def __sub__(self: Self, other: _VectorBase) -> vec3:
         if not isinstance(other, vec3):
             return NotImplemented
         return vec3(self.x - other.x, self.y - other.y, self.z - other.z)
+
+
+@dataclass(frozen=True, slots=True)
+class vec4(_VectorBase):
+    z: float
+    a: float
+
+    @property
+    def array(self: Self) -> list[float]:
+        return [self.x, self.y, self.z, self.a]
+
+    @staticmethod
+    def from_np(array: npt.NDArray[numpy.float64]) -> vec4:
+        assert isinstance(array, numpy.ndarray)
+        assert array.shape == (4,)
+        assert array.dtype == float
+        return vec4(x=float(array[0]), y=float(array[1]), z=float(array[2]), a=float(array[3]))
+
+    def __add__(self: Self, other: _VectorBase) -> vec4:
+        if not isinstance(other, vec4):
+            return NotImplemented
+        return vec4(self.x + other.x, self.y + other.y, self.z + other.z, self.a + other.a)
+
+    def __sub__(self: Self, other: _VectorBase) -> vec4:
+        if not isinstance(other, vec4):
+            return NotImplemented
+        return vec4(self.x - other.x, self.y - other.y, self.z - other.z, self.a - other.a)
