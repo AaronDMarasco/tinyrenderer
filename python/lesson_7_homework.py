77267735da07a4216fd9bb4a0e48165fa352ea5f
Aaron D. Marasco <AaronDMarasco@users.noreply.github.com>
2026-08-27 15:30:48 -0400

    Moved the reflection stuff around - capture more in the constructor. Math was close.

--- python/lesson_7_homework.py
+++ python/lesson_7_homework.py
@@ -1,6 +1,5 @@
 from __future__ import annotations
 
-import math
 import sys
 from pathlib import Path
 from typing import Final, Self
@@ -17,19 +16,42 @@ height: Final = 800
 
 eye: Final = vec3(-1, 0, 2)  # Camera position
 center: Final = vec3(0, 0, 0)  # Camera direction
-up: Final = vec3(0.5, 0.5, 0)  # Camera up vector
-sun: Final = vec3(0, -0, 100)  # Sun location
+up: Final = vec3(0, 1, 0)  # Camera up vector
+sun: Final = vec3(0, 0, 1)  # Sun location
 
 
 class PhongShader(our_gl.IShader):
     model: OBJ_Data
     color: TGAColor_t
     tri: list[vec3]  # Triangle in eye coordinates
+    # These are for reflection stuff:
+    sun_vector_l: vec4
+    ambient: float
+    diffuse_weight: float
+    specular_shine: int
 
-    def __init__(self: Self, model: OBJ_Data) -> None:
+    def __init__(
+        self: Self,
+        model: OBJ_Data,
+        *,
+        sun: vec3,
+        ambient: float = 0.5,
+        diffuse_weight: float = 0.4,
+        specular_weight: float = 0.9,
+        specular_shine: int = 3,
+    ) -> None:
+        assert 0 <= ambient <= 1, "Ambient should be 0..1 inclusive"
+        assert 0 <= diffuse_weight <= 1, "Diffuse term weight should be 0..1 inclusive"
+        assert 0 <= specular_weight <= 1, "Specular term weight should be 0..1 inclusive"
+
         self.model = model
         self.color = TGAColor()
         self.tri = [vec3(x=0, y=0, z=0), vec3(x=0, y=0, z=0), vec3(x=0, y=0, z=0)]
+        self.sun_vector_l = vec4.from_np(our_gl.model_view @ vec4.from_vec3(sun, w=0)).normalized
+        self.ambient = ambient
+        self.diffuse_weight = diffuse_weight
+        self.specular_weight = specular_weight
+        self.specular_shine = specular_shine
 
     def vertex(self: Self, face: int, vert: int) -> vec4:
         v: Final[vec3] = self.model.vert(face, vert)  # current vertex in object coordinates
@@ -37,52 +59,31 @@ class PhongShader(our_gl.IShader):
         self.tri[vert] = gl_position.xyz  # in eye coordinates
         return vec4.from_np(our_gl.perspective @ gl_position)  # in clip coordinates
 
-    def update_color(
-        self: Self,
-        face: int,
-        *,
-        sun: vec3,
-        ambient: float = 0.5,
-        diffuse_weight: float = 1.0,
-        specular_weight: float = 1.0,
-        specular_shine: int = 3,
-    ) -> None:
-        """Update color based on face"""
-        assert 0 <= ambient <= 1, "Ambient should be 0..1 inclusive"
-        assert 0 <= diffuse_weight <= 1, "Diffuse term weight should be 0..1 inclusive"
-        assert 0 <= specular_weight <= 1, "Specular term weight should be 0..1 inclusive"
-
-        # To get an orthogonal vector you need two vectors on the plane...
-        this_plane_points: Final[tuple[vec3, ...]] = tuple(self.model.vert(face, i) for i in range(3))
-        this_plane_vectors: Final[tuple[vec3, vec3]] = (
-            this_plane_points[1] - this_plane_points[0],
-            this_plane_points[2] - this_plane_points[0],
-        )
-        orthogonal_vector: Final[vec3] = vec3.from_np(np.cross(*this_plane_vectors)).normalized
-
-        # Is this needed???
-        # sun_position: Final = vec4.from_np(our_gl.model_view @ vec4.from_vec3(sun, w=1)).normalized
-        sun_position = vec4.from_vec3(sun.normalized, w=1)
+    def fragment(self: Self, _bar: vec3) -> tuple[bool, TGAColor_t]:
+        # To get an orthogonal vector (normal) you need two vectors on the plane...
+        this_plane_vectors: Final[tuple[vec3, vec3]] = (self.tri[1] - self.tri[0], self.tri[2] - self.tri[0])
+        triangle_normal_vector_n: Final[vec3] = vec3.from_np(np.cross(*this_plane_vectors)).normalized
 
         # Compute 0..1 for diffuse term
-        diffuse: Final = max(0, sun_position.xyz * orthogonal_vector)
-        alpha: Final[float] = math.acos(diffuse)
-        assert 0 <= diffuse <= 1, "'diffuse' should be 0..1 inclusive?"
+        diffuse: Final = max(0, self.sun_vector_l.xyz * triangle_normal_vector_n)
+        assert 0 <= diffuse <= 1, f"'{diffuse=}' should be 0..1 inclusive?"
 
         # Compute 0..1 for specular term
-        alpha_plus_beta: Final[float] = math.acos(eye.normalized * orthogonal_vector)
-        beta: Final = alpha_plus_beta - alpha
-        specular: Final = pow(max(0, math.cos(beta)), specular_shine)
-        assert 0 <= specular <= 1, "'specular' should be 0..1 inclusive?"
+        vector_r: Final[vec3] = (
+            triangle_normal_vector_n * (triangle_normal_vector_n * self.sun_vector_l.xyz) * 2 - self.sun_vector_l.xyz
+        ).normalized
+        # specular intensity - note that the camera lies on the z-axis (in eye coordinates),
+        # therefore simple r.z, since (0,0,1)*(r.x, r.y, r.z) = r.z
+        specular: Final = pow(max(0, vector_r.z), self.specular_shine)
+        assert 0 <= specular <= 1, f"'{specular=}' should be 0..1 inclusive?"
 
         # Now combine them (should be 0..3)
-        final = ambient + diffuse * diffuse_weight + specular * specular_weight
-        assert 0 <= final <= 3, "Final color should be 0..3 inclusive?"
+        final = self.ambient + diffuse * self.diffuse_weight + specular * self.specular_weight
+        assert 0 <= final <= 3, f"Final color {final=} should be 0..3 inclusive?"
 
         # Now scale it to 255
         self.color = TGAColor(round(final * 85))
 
-    def fragment(self: Self, _bar: vec3) -> tuple[bool, TGAColor_t]:
         return (False, self.color)  # do not discard the pixel
 
 
@@ -98,10 +99,6 @@ def main() -> int:
 ../obj/floor.obj
 ../obj/diablo3_pose/diablo3_pose.obj
 """
-    find_output = """
-../obj/african_head/african_head.obj
-../obj/diablo3_pose/diablo3_pose.obj
-"""
     our_gl.lookat(eye, center, up)  # build global model_view
     our_gl.init_perspective((eye - center).norm)  # build global persepctive
     our_gl.init_viewport(width // 16, height // 16, width * 7 // 8, height * 7 // 8)  # build global view_port
@@ -112,15 +109,13 @@ def main() -> int:
             framebuffer = TGAImage(width, height, TGAImage.Format.GRAYSCALE, TGAColor(255 // 2))
             our_gl.init_zbuffer(width, height)  # New zbuffer per image
             model = OBJ_Data.from_file(fname)
-            shader = PhongShader(model)
+            shader = PhongShader(model, sun=sun, specular_shine=35)
             for face in range(len(model.faces)):
-                # shader.color = TGAColor_t.random()
                 clip: Triangle = (  # assemble the primitive
                     shader.vertex(face, 0),
                     shader.vertex(face, 1),
                     shader.vertex(face, 2),
                 )
-                shader.update_color(face, sun=sun, specular_shine=64)
                 our_gl.rasterize(clip, shader, framebuffer)  # rasterize the primitive
 
             framebuffer.write_tga_file(f"{basename}.tga")
